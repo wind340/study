a404e13913e1cf64d4a1daca9a878c1a1912f078
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-02-08 17:35:52 +0900

    script practice

--- study_reactjs/app.js
+++ study_reactjs/app.js
@@ -1,7 +1,45 @@
-const age = 96;
-function calculateKrAge(ageOfForeugner){
-   return ageOfForeugner +2;
+const h1 = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick(){
+   const currentColor = h1.style.color;
+   let newColor;
+   if(currentColor === "blue"){
+      newColor = "tomato";      
+   }else{
+      newColor = "blue";
+   }
+   h1.style.color=newColor
 }
 
-const krAge = calculateKrAge(age);
-console.log(krAge);
+function handleMouseEnter(){
+   h1.innerText ="Mouse is here!~";
+}
+
+function handleMouseLeave(){
+   h1.innerText = "Mouse is gone";
+}
+
+function handleWindowResize(){
+   document.body.style.backgroundColor = "tomato";   
+}
+
+function handleWindowCopy(){
+   alert("copier");
+}
+
+function handleWindowOffline(){
+   alert("SOS no WIFI")
+}
+
+function handleWindowOnline(){
+   alert("Good on WIFI")
+}
+
+h1.addEventListener("click", handleTitleClick);
+h1.addEventListener("mouseenter", handleMouseEnter);
+h1.addEventListener("mouseleave", handleMouseLeave);
+
+window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offline", handleWindowOffline);
+window.addEventListener("online", handleWindowOnline);
