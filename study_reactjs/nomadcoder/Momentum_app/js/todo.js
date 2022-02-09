a71f3ecb245fe0faec124f3971ef2227c3a126de
Jungchul_kim <83698052+wind340@users.noreply.github.com>
2022-02-09 23:42:55 +0900

    momentum

---
+++ study_reactjs/nomadcoder/Momentum_app/js/todo.js
@@ -0,0 +1,30 @@
+const toDoForm = document.getElementById("todo-form");
+const toDoInput = document.querySelector("#todo-form input");
+const toDoList = document.getElementById("todo-list");
+
+function deleteTodo(event){
+    const li = event.target.parentElement;
+    li.remove();
+}
+
+
+function paintToDo(newTodo){
+    const li = document.createElement("li");
+    const span = document.createElement("span");
+    const button = document.createElement("button");
+    button.innerText = "❌";
+    button.addEventListener("click", deleteTodo);
+    li.appendChild(span);
+    li.appendChild(button);
+    span.innerText = newTodo;
+    toDoList.appendChild(li);
+}
+
+function handleToDoSubmit(event){
+    event.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value = "";
+    paintToDo(newTodo);
+}
+
+toDoForm.addEventListener("submit", handleToDoSubmit);
